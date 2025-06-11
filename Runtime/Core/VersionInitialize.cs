64e7f3f0a81010fd9e5925c172014d26a8c8912e
Valdeci Danilo <valdeci.danilo@gdstecnologia.com.br>
2025-06-11 11:20:17 -0300

    fix: update version
    Co-Authored-By: Leniton <48127860+Leniton@users.noreply.github.com>

--- Runtime/Core/VersionInitialize.cs
+++ Runtime/Core/VersionInitialize.cs
@@ -8,17 +8,22 @@ namespace CustomVersion.Core
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         private static void InitializeCanvasVersion()
         {
-            var prefab = Resources.Load<GameObject>("CanvasVersion");
-            if (prefab == null) return;
-
-            var canvasInst = Object.Instantiate(prefab);
-            canvasInst.name = "CanvasVersion";
+            var canvasInst =new GameObject("CanvasVersion").AddComponent<Canvas>();
+            canvasInst.renderMode = RenderMode.ScreenSpaceOverlay;
             Object.DontDestroyOnLoad(canvasInst);
 
-            var versionTextComp = canvasInst.transform.Find("Version_Text")?.GetComponent<TMP_Text>();
+            var versionTextComp = new GameObject("Version_Text").AddComponent<TextMeshProUGUI>();
+            versionTextComp.transform.SetParent(canvasInst.transform);
+            versionTextComp.alignment = TextAlignmentOptions.MidlineRight;
+            var rectText = (RectTransform) versionTextComp.GetComponent<Transform>();
+            rectText.anchorMin = Vector2.right;
+            rectText.anchorMax = Vector2.right;
+            rectText.pivot = Vector2.right;
+            rectText.sizeDelta = new Vector2(1058f, 50f);
+            rectText.anchoredPosition = Vector2.zero;
             if (versionTextComp == null) return;
 
-            var loader = canvasInst.AddComponent<VersionLoader>();
+            var loader = canvasInst.gameObject.AddComponent<VersionLoader>();
             loader.Init(versionTextComp);
         }
     }
