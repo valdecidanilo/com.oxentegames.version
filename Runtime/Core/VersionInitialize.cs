e6f8c7b4fedca9bfda57183ea6f7f87fac4c10db
Valdeci Danilo <valdeci.danilo@gdstecnologia.com.br>
2025-06-11 16:39:58 -0300

    fix: update preserve package script

--- Runtime/Core/VersionInitialize.cs
+++ Runtime/Core/VersionInitialize.cs
@@ -1,15 +1,18 @@
 ﻿using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Scripting;
+
 namespace CustomVersion.Core
 {
+    [Preserve]
     public abstract class VersionInitialize
     {
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         private static void InitializeCanvasVersion()
         {
             TMP_Settings.LoadDefaultSettings();
-            
+
             var jsonFile = Resources.Load<TextAsset>("version");
             if (jsonFile == null)
             {
@@ -26,12 +29,12 @@ namespace CustomVersion.Core
             canvas.vertexColorAlwaysGammaSpace = true;
             canvasGo.AddComponent<CanvasScaler>().uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
             canvasGo.AddComponent<ShowVersion>();
-            
+
             var textGo = new GameObject("VersionText");
             textGo.transform.SetParent(canvasGo.transform, false);
             var text = textGo.AddComponent<TextMeshProUGUI>();
-            if(versionData.environment != "release") text.text = $"v{versionData.release}.{versionData.build}-{versionData.environment}";
-            else if(versionData.environment == string.Empty) text.text = $"v{versionData.release}.{versionData.build}";
+            if (versionData.environment != "release") text.text = $"v{versionData.release}.{versionData.build}-{versionData.environment}";
+            else if (versionData.environment == string.Empty) text.text = $"v{versionData.release}.{versionData.build}";
             text.fontSize = 15;
             text.alignment = TextAlignmentOptions.BottomRight;
 
@@ -41,7 +44,7 @@ namespace CustomVersion.Core
             rect.pivot = new Vector2(1, 0);
             rect.anchoredPosition = new Vector2(-10, 5);
             rect.sizeDelta = new Vector2(500, 20);
-            
+
             Object.DontDestroyOnLoad(canvasGo);
         }
     }
