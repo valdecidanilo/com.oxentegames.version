84132a5a9c1ce04ad63507166b57afe66899b21f
Valdeci Danilo <valdeci.danilo@gdstecnologia.com.br>
2025-06-11 16:03:47 -0300

    fix: update using version

--- Runtime/Core/ShowVersion.cs
+++ Runtime/Core/ShowVersion.cs
@@ -4,6 +4,7 @@ public class ShowVersion : MonoBehaviour
 {
     private void Start()
     {
-
+        var version = Resources.Load<TextAsset>("version");
+        var json = JsonUtility.FromJson<VersionData>(version.text);
     }
 }
